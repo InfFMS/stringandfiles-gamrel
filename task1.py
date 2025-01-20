1d3de5004c295be8ce94c545f7e6f613bbacf383
gamrel <sockol1gor@yandex.ru>
2025-01-20 14:03:34 +0700

    ready
    3 remake

--- task1.py
+++ task1.py
@@ -4,3 +4,24 @@
 # Общее количество слов во всем тексте файла.
 # Общее количество символов (включая пробелы).
 # Выведите полученную статистику на экран.
+x=0
+y=0
+z=0
+with open('task1.txt', "r", encoding='utf-8') as f:
+   file=f.read()
+
+line_count = sum(1 for line in open('task1.txt'))
+for line in file:
+   x += len(line.split())
+for char in file:
+   if char != ' ':
+      z+=1
+
+
+#print(x)
+#print(z)
+print('Средняя длина слова в русской литературе - 6 символов. 152/6 =', x/6, 'что близко к правде)')
+print('Кол.во строк:',line_count)
+print('кол.во слов во всем тексте файла:',len(file)-line_count-z)
+print('Общее количество символов:',len(file)) #с учетом /n
+#print((file))
--- task2.py
+++ task2.py
@@ -3,3 +3,17 @@
 # Найдите и замените все вхождения слова "Python" на слово "Питон" (регистр учитывать).
 # Запишите обновленный текст в новый файл с другим именем.
 # Выведите на экран сообщение о количестве произведённых замен.
+a=0
+with open('task2.txt', "r", encoding='utf-8') as f:
+   file=f.read()
+#print((file))
+w='Python'
+c = file.count(w)
+new = file.replace('Python', 'Питон')
+
+with open ('test.txt', 'w', encoding='utf-8') as f:
+  f.write(new)
+with open('test.txt', "r", encoding='utf-8') as f:
+      file = f.read()
+print((file))
+print('количестве произведённых замен:',c)
